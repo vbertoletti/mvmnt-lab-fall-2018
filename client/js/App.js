f1f5bb6f55b4af204b516d999eae352e0ad608b8
Jenny rose Gutierrez <Phichik@phinchik.local>
2018-12-05 13:15:25 -0800

    add linear-gradient

--- client/js/App.js
+++ client/js/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+<<<<<<< HEAD
 import client from "./config/api";
 import { ApolloProvider } from "react-apollo";
 import { UserProvider } from "./context/UserContext";
 import AppNavigator from "./navigation/RootStackNavigator";
+=======
+import { Text, View } from "react-native";
+import { typography } from "../../client/js/config/styles";
+>>>>>>> add linear-gradient
 
 export default class App extends Component {
   render() {
