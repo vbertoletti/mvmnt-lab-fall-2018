531990e4983aec967ec54b8433a2c5728a47a724
Jenny rose Gutierrez <Phichik@phinchik.local>
2018-12-05 13:15:25 -0800

    add linear-gradient

--- client/js/App.js
+++ client/js/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import { typography } from "../../client/js/config/styles";
 
 export default class App extends Component {
   render() {
