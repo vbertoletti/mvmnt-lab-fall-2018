b19fcd658928ca00118c79cbe8132fb7345f0d23
Jenny rose Gutierrez <Phichik@phinchik.local>
2018-12-06 15:17:50 -0800

    set up the mainpage ui and install and link vector icon

--- client/js/screens/MainPage/MainPage.js
+++ client/js/screens/MainPage/MainPage.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 import propTypes from "prop-types";
@@ -19,6 +20,28 @@ const MainPage = ({ navigation }) => {
 
 MainPage.propTypes = {
   navigation: propTypes.object.isRequired
+=======
+import React, { Fragment } from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+import { ImageBackground } from "react-native";
+import MainPageSessionCard from "../../components/MainPageSessionCard";
+
+const MainPage = () => {
+  return (
+    <Fragment>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../../assets/images/BigHeader.png")}
+          style={styles.backgroundImage}
+        >
+          <Text style={styles.text}>GROUND CONTROL</Text>
+        </ImageBackground>
+        <MainPageSessionCard style={styles.card} />
+      </View>
+    </Fragment>
+  );
+>>>>>>> set up the mainpage ui and install and link vector icon
 };
 
 export default MainPage;
