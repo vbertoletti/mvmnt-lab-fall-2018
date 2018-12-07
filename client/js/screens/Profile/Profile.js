de662c7c3709453e5d55dff15fd919f904db6139
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-12-07 11:44:12 -0800

    resolve merge conflicts, npm install on server side and re-add profile page styles and markup

--- client/js/screens/Profile/Profile.js
+++ client/js/screens/Profile/Profile.js
@@ -1,59 +1,41 @@
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import styles from "./styles";
 import React from "react";
-import { Text,TouchableOpacity } from "react-native";
-import propTypes from "prop-types";
 
-const Profile = ({ navigation }) => {
+const Profile = () => {
   return (
-    <React.Fragment>
-      <Text>I am the Profile Screen</Text>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("EditProfile");
-        }}
-      >
-        <Text>Edit Profile</Text>
+    <View style={styles.profileWrapper}>
+      <View style={styles.profileHeader}>
+        <View style={styles.headerItems}>
+          <Image
+            source={require("../../assets/images/placeholder.jpg")}
+            style={styles.profileImage}
+          />
+          <Text style={styles.headerItemsText}>Person Name</Text>
+        </View>
+        <View style={styles.headerItems}>
+          <View style={styles.progression}>
+            <Text style={styles.progressionText}>7/30</Text>
+          </View>
+          <Text style={styles.headerItemsText}>Progress</Text>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.profileOptions}>
+        <Text style={styles.profileOptionsText}>Daily Reports</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("DailyReports");
-        }}
-      >
-        <Text>To DailyReports</Text>
+      <TouchableOpacity style={styles.profileOptions}>
+        <Text style={styles.profileOptionsText}>Completed Challenges</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("CompletedChalleneges");
-        }}
-      >
-        <Text>To CompletedChallenges</Text>
+      <TouchableOpacity style={styles.profileOptions}>
+        <Text style={styles.profileOptionsText}>Reminders</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("Reminders");
-        }}
-      >
-        <Text>To Reminders</Text>
+      <TouchableOpacity style={styles.profileOptions}>
+        <Text style={styles.profileOptionsText}>About</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("About");
-        }}
-      >
-        <Text>To About</Text>
+      <TouchableOpacity style={styles.profileButton}>
+        <Text style={styles.buttonText}>LOGOUT</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("SignIn");
-        }}
-      >
-        <Text>logout</Text>
-      </TouchableOpacity>
-    </React.Fragment>
+    </View>
   );
 };
-
-Profile.propTypes = {
-  navigation: propTypes.object.isRequired
-};
-
 export default Profile;
--- client/js/screens/Profile/styles.js
+++ client/js/screens/Profile/styles.js
@@ -1,9 +1,52 @@
 import { StyleSheet } from "react-native";
 import { globalStyles } from "../../config/styles";
-
+import { Dimensions } from "react-native";
 const styles = StyleSheet.create({
-  container: globalStyles.container
+  container: globalStyles.container,
+  loginForm: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  formFields: {
+    color: "white"
+  },
+  loginButtons: {
+    alignSelf: "center",
+    color: "white",
+    margin: 15
+  },
+  labels: {
+    color: "white"
+  },
+  fieldLines: {
+    borderBottomWidth: 1,
+    borderBottomColor: "white",
+    marginBottom: 20,
+    marginTop: 5
+  },
+  button: {
+    width: 300,
+    borderRadius: 30,
+    alignSelf: "center",
+    backgroundColor: "#1BC695"
+  },
+  background: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+  },
+  textInput: {
+    color: "white",
+    height: 30
+  },
+  inputFields: {
+    color: "white"
+  },
+  companyLogo: {
+    width: 210,
+    height: 100,
+    alignSelf: "center",
+    marginTop: 60
+  }
 });
-
-
 export default styles;
