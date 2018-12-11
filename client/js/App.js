58bd4b670873d8458abd662392fcf1ffefabb15b
Emilio Lombana <emiliolombana@gmail.com>
2018-12-11 15:47:46 -0800

    Revert app.js to return AppNavigator

--- client/js/App.js
+++ client/js/App.js
@@ -3,14 +3,13 @@ import client from "./config/api";
 import { ApolloProvider } from "react-apollo";
 import { UserProvider } from "./context/UserContext";
 import AppNavigator from "./navigation/RootStackNavigator";
-import ForgotPassword from '../js/screens/ForgotPassword'
 
 export default class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <UserProvider>
-          <ForgotPassword />
+          <AppNavigator />
         </UserProvider>
       </ApolloProvider>
     );
