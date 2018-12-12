4a188220e02933dbef21f2bd74b71e4cdeb168c5
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-12-11 21:01:07 -0800

    work in progress for the signout call to realm method

--- client/js/App.js
+++ client/js/App.js
@@ -3,13 +3,15 @@ import client from "./config/api";
 import { ApolloProvider } from "react-apollo";
 import { UserProvider } from "./context/UserContext";
 import AppNavigator from "./navigation/RootStackNavigator";
+import Profile from "./screens/Profile";
 
 export default class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <UserProvider>
-          <AppNavigator />
+          <Profile />
+          {/* <AppNavigator /> */}
         </UserProvider>
       </ApolloProvider>
     );
