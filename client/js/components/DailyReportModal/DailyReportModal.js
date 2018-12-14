15a3431c26ac998697abde35a636eb337ad71596
Owen Delisle <owen37164@gmail.com>
2018-12-14 10:13:11 -0800

    Removed console logs, and created seperate query file

--- client/js/components/DailyReportModal/DailyReportModal.js
+++ client/js/components/DailyReportModal/DailyReportModal.js
@@ -8,7 +8,9 @@ class DailyReportModal extends Component {
     super(props);
     this.allChallenges = [];
     this.date = new Date().toISOString();
+    this.pain = 0;
     this.score = 2;
+    this.work = 2;
     this.scores = [];
   }
 
@@ -30,13 +32,6 @@ class DailyReportModal extends Component {
   }
 
   render() {
-    console.log(
-      this.getDaysBetween(
-        new Date().toISOString(),
-        moment(new Date().toISOString()).add(1, "Y")
-      )
-    );
-
     return (
       <View style={styles.centerContainer}>
         <Text>User ID: {this.props.userId}</Text>
@@ -45,11 +40,11 @@ class DailyReportModal extends Component {
             this.props.createReport({
               variables: {
                 date: new Date().toISOString(),
-                pain: 0,
-                score: 2,
-                work: 2,
+                pain: this.pain,
+                score: this.score,
+                work: this.work,
                 userId: this.props.userId,
-                painDescription: "None felt great",
+                painDescription: "Test pain description",
                 notes: "Im dying"
               }
             });
@@ -61,7 +56,7 @@ class DailyReportModal extends Component {
                 challenge.endDate >= new Date().toISOString()
               ) {
                 this.scores = challenge.score;
-                this.scores.push(this.score);
+                this.scores.push(this.work);
                 this.props.updateChallenge({
                   variables: { id: challenge.id, score: this.scores }
                 });
--- client/js/components/DailyReportModal/DailyReportModalContainer.js
+++ client/js/components/DailyReportModal/DailyReportModalContainer.js
@@ -1,77 +1,18 @@
 import React, { Component } from "react";
-import gql from "graphql-tag";
 import UserContext from "../../context/UserContext/UserProvider";
 import DailyReportModal from "./DailyReportModal";
 import { graphql, compose } from "react-apollo";
-
-const CreateDailyReport = gql`
-  mutation CreateDailyReport(
-    $date: DateTime!
-    $pain: Int!
-    $score: Float!
-    $work: Int!
-    $userId: String!
-    $painDescription: String!
-    $notes: String!
-  ) {
-    createDailyReport(
-      date: $date
-      pain: $pain
-      score: $score
-      userId: $userId
-      work: $work
-      painDescription: $painDescription
-      notes: $notes
-    ) {
-      id
-    }
-  }
-`;
-
-const CreateChallengeMutation = gql`
-  mutation CreateChallenge(
-    $endDate: DateTime!
-    $startDate: DateTime!
-    $score: [Float!]
-    $userId: String!
-    $daysBetween: Int!
-  ) {
-    createChallenge(
-      endDate: $endDate
-      startDate: $startDate
-      score: $score
-      userId: $userId
-      daysBetween: $daysBetween
-    ) {
-      id
-    }
-  }
-`;
-
-const UpdateChallengeMutation = gql`
-  mutation UpdateChallenge($id: ID!, $score: [Float!]) {
-    updateChallenge(id: $id, score: $score) {
-      id
-    }
-  }
-`;
-
-const AllChallengesQuery = gql`
-  query AllChallenges {
-    allChallenges {
-      id
-      startDate
-      endDate
-      score
-    }
-  }
-`;
+import { CreateDailyReport } from "../../apollo";
+import { CreateChallengeMutation } from "../../apollo";
+import { UpdateChallengeMutation } from "../../apollo";
+import { AllChallengesQuery } from "../../apollo";
 
 class DailyReportModalContainer extends Component {
   render() {
     return (
       <UserContext.Consumer>
-        {({}) => {
+        {id => {
+          console.log("ID", id);
           return (
             <DailyReportModal
               createReport={this.props.createReport}
