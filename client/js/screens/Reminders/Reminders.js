bfa15f44692c146941fba72ceac4179620001f69
Emilio Lombana <emiliolombana@gmail.com>
2018-12-18 15:44:15 -0800

    Solving merge conflicts

--- client/js/screens/Reminders/Reminders.js
+++ client/js/screens/Reminders/Reminders.js
@@ -1,12 +1,49 @@
 import React from "react";
-import { Text } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  ScrollView
+} from "react-native";
+import ReminderSwitch from '../../components/ReminderSwitch'
+import styles from "./styles";
 
 const Reminders = () => {
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          backgroundColor: "#CED0CE"
+        }}
+      />
+    );
+  };
+  _keyExtractor = item => item.id;
   return (
-    <React.Fragment>
-      <Text>I am the Reminders Screen</Text>
-    </React.Fragment>
+    <ScrollView>
+      <FlatList
+        data={[
+          { id: "0", day: "Monday", time: "05:00 PM" },
+          { id: "1", day: "Tuesday", time: "05:00 PM" },
+          { id: "2", day: "Wednesday", time: "05:00 PM" },
+          { id: "3", day: "Thursday", time: "05:00 PM" },
+          { id: "4", day: "Friday", time: "05:00 PM" },
+          { id: "5", day: "Saturday", time: "05:00 PM" },
+          { id: "6", day: "Sunday", time: "05:00 PM" }
+        ]}
+        keyExtractor={this._keyExtractor}
+        ItemSeparatorComponent={this.renderSeparator}
+        renderItem={({ item }) => (
+          <View style={styles.Pose}>
+            <Text style={styles.image}>{item.day}</Text>
+            <Text style={styles.time}>{item.time}</Text>
+            <ReminderSwitch />
+          </View>
+        )}
+      />
+    </ScrollView>
   );
-};
+}
 
 export default Reminders;
